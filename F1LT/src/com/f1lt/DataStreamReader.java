253f5317223f438a9b4e6b935442d44373164e78
epastodi7 <fqe_servia@yahoo.es>
2013-04-20 14:26:35 +0200

    Pujat dades fins a la Qualy de Bahrain.

--- F1LT/src/com/f1lt/DataStreamReader.java
+++ F1LT/src/com/f1lt/DataStreamReader.java
@@ -409,7 +409,7 @@ public class DataStreamReader
 	        
 	        //Creem un arxiu per copiar a dins
 	        File sdCard = Environment.getExternalStorageDirectory();
-	        File dir = new File (sdCard.getAbsolutePath() + "/PROVA/F1/BAH/P1-1");
+	        File dir = new File (sdCard.getAbsolutePath() + "/PROVA/F1/BAH/R-1");
 	        String nom = "Dades";
 	        nom=nom.concat(Integer.toString(blocks));
 	        nom=nom.concat(".txt");
